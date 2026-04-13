96e59fcfb5194ecd2447ca8c975adfaa93f9b0a5
Vincenzo <Vincenzo_Piccolo@Outlook.it>
2026-04-13 14:54:15 +0200

    Add functionality to display and manage user persona details

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  standalone : true,
-  imports: [FormsModule,CommonModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './user.html',
-  styleUrl: './user.css',
+  styleUrls: ['./user.css'],
 })
 export class UserCompont implements OnInit {
 
@@ -15,25 +15,40 @@ export class UserCompont implements OnInit {
 
   numero : number = 0;
 
+  bool : boolean = true ;
+
   lista: number[] = [];
+
+  personList : Person[] = [];
+
+  persona : Person = new Person("Mario","Rossi")
 
   constructor() {
   }
 
-
    ngOnInit() {
 
   }
-
 
   aggiungi1(){
    this.numero = this.numero+1;
    this.lista.push(this.numero)
   }
 
-
   rimuovi1(){
    this.lista.pop()
   }
 
+}
+
+export class Person {
+
+  nome!: string;
+
+  cognome !: string;
+
+  constructor(nome : string, cognome: string){
+    this.nome = nome;
+    this.cognome = cognome;
+  }
 }
