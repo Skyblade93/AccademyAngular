1361c50b34d7b95fc489341ce56463aaf8ce460d
Vincenzo <Vincenzo_Piccolo@Outlook.it>
2026-04-14 17:53:11 +0200

    fix: update navigation and routing for Notifiche component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,11 +11,9 @@ import { AutoComponent } from './auto-component/auto-component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path : 'user', component : UserComponent},
   {path : 'drone', component : DroneComponent},
-  {path : 'home' , component: HomeComponent},
-  {path : '**' , redirectTo:'home'}
   { path: 'auto', component: AutoComponent },
+  { path: 'notifica', component: NotificaComponent },
   { path: 'user', component: UserComponent },
   { path: 'home', component: HomeComponent },
   {path : 'dipendente' , component: DipendenteComponent},
