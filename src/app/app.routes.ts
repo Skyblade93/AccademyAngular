5151c5435ea07883288421b11e8af4270d0377d6
giova <giovanni14.piccolo03@gmail.com>
2026-04-14 14:46:24 +0200

    add: carrello: carrello.html, carrello.css, carrello.ts, carrelloDto.ts, carrelloService.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
+import { Carrello } from './carrello/carrello';
 import { HomeComponent } from './home/home';
 import { UserComponent } from './user/user';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path : 'user', component : UserComponent},
-  {path : 'home' , component: HomeComponent},
-  {path : '**' , redirectTo:'home'}
+  { path: 'user', component: UserComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'carrello', component: Carrello },
+  { path: '**', redirectTo: 'home' },
 ];
