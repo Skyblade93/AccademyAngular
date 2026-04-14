01d33e185d0eb4e2c64fdb21b9a0bef49accf478
Mor33mila <lorenzolomb20@gmail.com>
2026-04-14 16:54:28 +0200

    CreazioneAuto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Carrello } from './carrello/carrello';
 import { HomeComponent } from './home/home';
 import { NotificaComponent } from './notifica/notifica';
 import { UserComponent } from './user/user';
+import { AutoComponent } from './auto-component/auto-component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: 'auto', component: AutoComponent },
   { path: 'user', component: UserComponent },
   { path: 'home', component: HomeComponent },
   {path : 'azienda', component: AziendaComponent},
---
+++ src/app/auto-component/auto-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AutoComponent } from './auto-component';
+
+describe('AutoComponent', () => {
+  let component: AutoComponent;
+  let fixture: ComponentFixture<AutoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AutoComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AutoComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
