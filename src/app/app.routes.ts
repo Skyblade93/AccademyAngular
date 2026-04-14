3c8babe4c10438ec8adaf49a37dbe3e368b3f1f6
giacopa97 <giacopa97@gmail.com>
2026-04-14 16:44:44 +0200

    First Insert Front-End Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
+import { AziendaComponent } from './azienda/azienda';
+import { Carrello } from './carrello/carrello';
 import { HomeComponent } from './home/home';
 import { NotificaComponent } from './notifica/notifica';
 import { UserComponent } from './user/user';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path : 'user', component : UserComponent},
-  {path : 'home' , component: HomeComponent},
-  {path : '**' , redirectTo:'home'}
+  { path: 'user', component: UserComponent },
+  { path: 'home', component: HomeComponent },
+  {path : 'azienda', component: AziendaComponent},
+  { path: 'carrello', component: Carrello },
+  { path: '**', redirectTo: 'home' },
 ];
