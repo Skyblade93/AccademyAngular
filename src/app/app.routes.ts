27dd12416cb4d9a03cf7e01ccd615c3259a66565
stefano7703 <162974312+stefano7703@users.noreply.github.com>
2026-04-14 16:37:16 +0200

    add: azienda aziendaDto serviceAzienda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { AziendaComponent } from './azienda/azienda';
 import { Carrello } from './carrello/carrello';
 import { HomeComponent } from './home/home';
 import { UserComponent } from './user/user';
@@ -6,6 +7,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'home', component: HomeComponent },
+  {path : 'azienda', component: AziendaComponent},
   { path: 'carrello', component: Carrello },
   { path: '**', redirectTo: 'home' },
 ];
