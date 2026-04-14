d6e87d55772eb740b0cee7d1d6939ca58798e550
giova <giovanni14.piccolo03@gmail.com>
2026-04-14 14:46:24 +0200

    add: carrello: carrello.html, carrello.css, carrello.ts, carrelloDto.ts, carrelloService.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
-import { AziendaComponent } from './azienda/azienda';
 import { HomeComponent } from './home/home';
 import { NotificaComponent } from './notifica/notifica';
 import { UserComponent } from './user/user';
@@ -6,13 +5,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {path : 'user', component : UserComponent},
-  {path : 'drone', component : DroneComponent},
   {path : 'home' , component: HomeComponent},
-  {path : '**' , redirectTo:'home'}
-  { path: 'auto', component: AutoComponent },
-  { path: 'user', component: UserComponent },
-  { path: 'home', component: HomeComponent },
-  {path : 'dipendente' , component: DipendenteComponent},
-  {path : 'azienda', component: AziendaComponent},
   {path : '**' , redirectTo:'home'}
 ];
