8947d46bce14306063f2181092188968976cea1a
Vincenzo <Vincenzo_Piccolo@Outlook.it>
2026-04-14 09:11:18 +0200

    Refactor user component: remove navbar styles, update button styles, and enhance form validation with required fields

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserDto } from '../home/Dto/UserDto';
 
 @Component({
@@ -20,13 +20,18 @@ export class UserComponent {
 
   lista: number[] = [];
 
+  listaArray : Array<number> = [];
+
   ListaPersona : UserDto[] = [];
 
  user : UserDto = new UserDto("mario", "rossi");
  user2 : UserDto = new UserDto("luigi", "verdi");
 
 userForm = new FormGroup({
-  nome: new FormControl(''),
+  nome: new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required]
+  }),
   cognome: new FormControl('')
 });
 
@@ -53,11 +58,35 @@ elimina(){
 
 
 onSubmit() {
-  const value = this.userForm.value;
+ if (this.userForm.valid) {
+  const value = this.userForm.getRawValue();
 
   this.ListaPersona.push(
-    new UserDto(value.nome!, value.cognome!)
+    new UserDto(value.nome, value.cognome || "")
   );
+}
+}
+
+ordinaLista(){
+  this.ListaPersona.sort((a,b) => a.cognome.localeCompare(b.cognome)).
+  forEach((persona) =>{
+    persona.nome = persona.nome.charAt(0).toUpperCase().concat(persona.nome.slice(1));
+    persona.cognome = persona.cognome.charAt(0).toUpperCase().concat(persona.cognome.slice(1));});
+
+}
+
+deletePersona(index : number){
+  this.ListaPersona.splice(index,1);
+}
+
+
+modificaPersona(index : number){
+  const persona = this.ListaPersona[index];
+  this.userForm.setValue({
+    nome: persona.nome,
+    cognome: persona.cognome
+  });
+  this.ListaPersona.splice(index,1);
 }
 
 }
