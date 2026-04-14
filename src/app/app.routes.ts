a50497be4bc31eefd753b193f24a5e51c309a14a
Mor33mila <lorenzolomb20@gmail.com>
2026-04-14 16:54:28 +0200

    CreazioneAuto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { AziendaComponent } from './azienda/azienda';
 import { Carrello } from './carrello/carrello';
 import { HomeComponent } from './home/home';
 import { UserComponent } from './user/user';
+import { AutoComponent } from './auto-component/auto-component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: 'auto', component: AutoComponent },
   { path: 'user', component: UserComponent },
   { path: 'home', component: HomeComponent },
   {path : 'dipendente' , component: DipendenteComponent},
