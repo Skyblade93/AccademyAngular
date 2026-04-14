0c84c702a0d51e828aa2d3bb6485b1c407fcacc1
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-04-14 14:09:28 +0200

    commit drone

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,18 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DroneService } from '../Service/drone.service'; // Controlla il percorso
+import { DroneDto } from '../Dto/DroneDto';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  // Questo array conterrà i droni che arrivano dal backend
+  listaDroni: DroneDto[] = [];
 
+  constructor(private droneService: DroneService) {}
 
-  numero : number = 0;
+  ngOnInit(): void {
+    // Appena la pagina si carica, leggiamo i droni
+    this.caricaDroni();
+  }
 
-  cambiaNumero(){
-    this.numero = this.numero +1 ;
+  caricaDroni(): void {
+    this.droneService.getAll().subscribe({
+      next: (data) => {
+        this.listaDroni = data;
+        console.log("Droni caricati:", data);
+      },
+      error: (err) => console.error("Errore nel caricamento droni", err)
+    });
+  }
 
+  salvaDrone(droneForm: any): void {
+    if (droneForm.valid) {
+      this.droneService.insert(droneForm.value).subscribe(() => {
+        this.caricaDroni(); // Ricarica la lista dopo l'inserimento
+        droneForm.reset();
+      });
+    }
+  }
+
+  eliminaDrone(id: number | undefined): void {
+    if (id) {
+      this.droneService.delete(id).subscribe(() => {
+        this.caricaDroni(); // Aggiorna la lista dopo l'eliminazione
+      });
+    }
   }
 }
