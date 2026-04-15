0deaa96fe94fc74eb4b72653c8a6c93d644ea63d
Vincenzo <Vincenzo_Piccolo@Outlook.it>
2026-04-15 16:06:25 +0200

    fix: reorder imports in app.routes.ts and update rootDir in tsconfig.spec.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { DipendenteComponent } from './dipendente/dipendente';
 import { AziendaComponent } from './azienda/azienda';
 import { Carrello } from './carrello/carrello';
-import { HomeComponent } from './home/home';
 import { DroneComponent } from './drone/drone';
 import { NotificaComponent } from './notifica/notifica';
-import { UserComponent } from './user/user';
 import { AutoComponent } from './auto-component/auto-component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { UserComponent } from './user/user';
 
 export const routes: Routes = [
+  {path : 'home', component : HomeComponent},
+  {path : 'user', component : UserComponent},
   {path : 'drone', component : DroneComponent},
   { path: 'auto', component: AutoComponent },
   { path: 'notifica', component: NotificaComponent },
