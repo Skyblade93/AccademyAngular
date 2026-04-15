8827112267f01bb0738db6ceb4ddde1a0cfed116
giacopa97 <giacopa97@gmail.com>
2026-04-15 09:03:04 +0200

    First Update Css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { AziendaComponent } from './azienda/azienda';
 import { Carrello } from './carrello/carrello';
 import { HomeComponent } from './home/home';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e1d30ac (First Update Css)
 import { DroneComponent } from './drone/drone';
 =======
 >>>>>>> 73d4986 (Modified: notifica)
@@ -21,9 +24,12 @@ export const routes: Routes = [
 =======
   { path: 'auto', component: AutoComponent },
   { path: 'notifica', component: NotificaComponent },
+<<<<<<< HEAD
   { path: 'user', component: UserComponent },
   { path: 'home', component: HomeComponent },
 >>>>>>> 1361c50 (fix: update navigation and routing for Notifiche component)
+=======
+>>>>>>> e1d30ac (First Update Css)
   {path : 'dipendente' , component: DipendenteComponent},
   {path : 'azienda', component: AziendaComponent},
   { path: 'carrello', component: Carrello },
