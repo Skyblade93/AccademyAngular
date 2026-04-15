8823142a97acdcbe8ebad1e0d4bfe8024484a685
Vincenzo <Vincenzo_Piccolo@Outlook.it>
2026-04-15 16:45:13 +0200

    edit home + edit style contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { ContactComponent } from './contact/contact';
 import { DipendenteComponent } from './dipendente/dipendente-component';
 import { AziendaComponent } from './azienda/azienda';
@@ -5,6 +6,11 @@ import { HomeComponent } from './home-component/home-component';
 import { DroneComponent } from './drone/drone';
 import { NotificaComponent } from './notifica-component/notifica-component';
 import { OrdineComponent } from './ordine-Component/ordine';
+=======
+
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home';
+>>>>>>> 5ac41b4 (edit home + edit style contact)
 import { UserComponent } from './user/user';
 import { AutoComponent } from './auto-component/auto-component';
 import { CarrelloComponent } from './carrello/carrello-component';
--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/app/home-component/home-component.ts
  import { Component } from '@angular/core';
 
 @Component({
@@ -5,14 +6,34 @@
   imports: [],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
+=======
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+>>>>>>> 5ac41b4 (edit home + edit style contact):src/app/home/home.ts
 })
+
 export class HomeComponent {
+  welcomeTitle = 'Academy Engineering Hub';
+  welcomeMessage =
+    'Il centro di controllo unificato per la gestione di logistica, utenza e infrastrutture intelligenti.';
 
+  isDetailsOpen = signal(false); // Stato per la tendina
 
-  numero : number = 0;
+  constructor(private router: Router) {}
 
-  cambiaNumero(){
-    this.numero = this.numero +1 ;
+  toggleDetails(): void {
+    this.isDetailsOpen.update((value) => !value);
+  }
 
+  goToContacts(): void {
+    this.router.navigate(['/contact']);
   }
 }
