dddf2813ea1a026897068a7bb9e90596629ffb6b
Vincenzo <Vincenzo_Piccolo@Outlook.it>
2026-04-15 10:24:12 +0200

    feat: enhance user component with form submission and user creation functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,33 +3,19 @@ import { DipendenteComponent } from './dipendente/dipendente';
 import { AziendaComponent } from './azienda/azienda';
 import { Carrello } from './carrello/carrello';
 import { HomeComponent } from './home/home';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e1d30ac (First Update Css)
+import { Routes } from '@angular/router';
+import { AutoComponent } from './auto-component/auto-component';
 import { DroneComponent } from './drone/drone';
-=======
->>>>>>> 73d4986 (Modified: notifica)
 import { NotificaComponent } from './notifica/notifica';
 import { UserComponent } from './user/user';
-import { AutoComponent } from './auto-component/auto-component';
-import { Routes } from '@angular/router';
+
 
 export const routes: Routes = [
   {path : 'drone', component : DroneComponent},
-<<<<<<< HEAD
-  {path : 'user', component : UserComponent},
   { path: 'auto', component: AutoComponent },
   { path: 'notifica', component: NotificaComponent },
-=======
-  { path: 'auto', component: AutoComponent },
-  { path: 'notifica', component: NotificaComponent },
-<<<<<<< HEAD
   { path: 'user', component: UserComponent },
   { path: 'home', component: HomeComponent },
->>>>>>> 1361c50 (fix: update navigation and routing for Notifiche component)
-=======
->>>>>>> e1d30ac (First Update Css)
   {path : 'dipendente' , component: DipendenteComponent},
   {path : 'azienda', component: AziendaComponent},
   { path: 'carrello', component: Carrello },
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,13 +1,7 @@
 import { userService } from './../Service/userService';
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit, signal } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserDto } from '../Dto/UserDto';
 
 @Component({
@@ -88,6 +82,7 @@ ottieniElemento(id: number) {
   ordinaListUser(){
     this.UserSignal.update((users) => users.sort((a, b) => a.id - b.id));
   }
+
 
 /*/* Metodo per gestire la chiusura del popup */
 togglePopup() {
