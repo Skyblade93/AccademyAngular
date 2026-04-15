da6597cdeb237757a8c1b38ad8409cdbf0d0b46c
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2026-04-15 16:08:27 +0200

    add: ordineDto ordine.css ordine.html ordine.spec.ts ordine.ts ordineService.ts modify: app.routes app.html

---
+++ src/app/Service/ordineService.ts
@@ -0,0 +1,60 @@
+
+import { OrdineDto } from "../Dto/OrdineDto";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+import { AbstractService } from "./abstract-service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ordineService extends AbstractService<OrdineDto>{
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.type = 'Ordine';
+     const baseProjectUrl = this.baseUrl + '/' + this.type;
+
+  }
+
+    findById(id: number): Observable<OrdineDto> {
+        return this.http.get<OrdineDto>(this.baseUrl + '/' + this.type + '/findById?id='+ id);
+    }
+
+    trovaConCostoUguale(costo: number): Observable<OrdineDto[]> {
+        return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/trovaConCostoUguale?costo='+ costo);
+    }
+
+    trovaConNumeroProdottiMaggiore(num_prodotti: number): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/trovaConNumeroProdottiMaggiore?num_prodotti='+ num_prodotti);
+  }
+
+  trovaConIndirizzo(indirizzo: string): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/trovaConIndirizzo?indirizzo=' + indirizzo);
+  }
+
+  trovaConCostoMaggiore(costo: number): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/trovaConCostoMaggiore?costo='+ costo);
+  }
+
+  trovaConCostoMinore(costo: number): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/trovaConCostoMinore?costo='+ costo);
+  }
+
+  trovaPerUtente(utente: number): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/trovaPerUtente?utente='+ utente);
+  }
+
+  filtro(numero: number, costo: number): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/filtro?numero='+ numero +'&costo='+ costo);
+  }
+
+  ordinaPerCostoDecrescente(): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/ordinaPerCostoDecrescente');
+  }
+
+  trovaTraDueCosti(min: number, max: number): Observable<OrdineDto[]> {
+    return this.http.get<OrdineDto[]>(this.baseUrl + '/' + this.type + '/trovaTraDueCosti?min='+ min +'&max='+ max);
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { HomeComponent } from './home/home';
+import { OrdineComponent } from './ordine-Component/ordine';
 import { UserComponent } from './user/user';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {path : 'user', component : UserComponent},
   {path : 'home' , component: HomeComponent},
+  {path : 'ordine' , component: OrdineComponent},
   {path : '**' , redirectTo:'home'}
+
 ];
