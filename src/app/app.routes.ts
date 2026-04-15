b394935de994e6c4dbb5e04402275e86875e9151
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-04-15 09:26:43 +0200

    Aggiunto componente Drone completo di grafica e logica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
-import { HomeComponent } from './home/home';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home'; // Aggiungi .ts se non lo trova
 import { DroneComponent } from './drone/drone';
 import { UserComponent } from './user/user';
-import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path : 'user', component : UserComponent},
-  {path : 'drone', component : DroneComponent},
-  {path : 'home' , component: HomeComponent},
-  {path : '**' , redirectTo:'home'}
+  { path: 'user', component: UserComponent },
+  { path: 'drone', component: DroneComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Meglio usare questo per la home
+  { path: '**', redirectTo: 'home' }
 ];
