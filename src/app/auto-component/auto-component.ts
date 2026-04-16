80b25e0095849c9d91a61ec334070d5b2c89b7df
Mor33mila <lorenzolomb20@gmail.com>
2026-04-16 11:30:21 +0200

    Mod. Auto.html/ts

--- src/app/auto-component/auto-component.ts
+++ src/app/auto-component/auto-component.ts
@@ -25,7 +25,6 @@ export class AutoComponent implements OnInit, OnDestroy {
   aziendaSrv: aziendaService;
   dipendenteSrv: DipendenteService;
 
-
   listAuto: AutoDto[] = [];
   userList: UserDto[] = [];
   aziendaList: AziendaDto[] = [];
