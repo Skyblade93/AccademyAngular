2d045318f437fc85e188cb163d718341ae0f5882
giulioborriello <75851725+giulioborriello@users.noreply.github.com>
2026-04-16 16:17:33 +0200

    aggiunto add on

--- src/app/notifica-component/notifica-component.ts
+++ src/app/notifica-component/notifica-component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { NotificaDto } from '../Dto/NotificaDto';
 import { DatePipe } from '@angular/common';
 import { NotificaService } from '../Service/NotificaService';
+import { AddNotificaComponent } from '../addOn/add-notifica-component/add-notifica-component';
 
 @Component({
   selector: 'app-notifica',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, AddNotificaComponent],
   templateUrl: './notifica-component.html',
   styleUrl: './notifica-component.css',
 })
-
 export class NotificaComponent implements OnInit {
   listNotifica: NotificaDto[] = [];
 
+  istoggleAddNotifica = signal(false);
 
-  constructor(private notificaService: NotificaService) {
-  }
+  constructor(private notificaService: NotificaService) {}
 
   ngOnInit(): void {
     this.caricaTutte();
+  }
+
+  toggleAddNotifica(): void {
+    this.istoggleAddNotifica.update(v => !v);
   }
 
   caricaTutte(): void {
