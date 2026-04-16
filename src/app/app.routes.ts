9c44cd99bae65c6570234f5ddcf0bc6b7686dd37
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-04-16 14:50:28 +0200

    Fix: pulizia rotte duplicate e risoluzione conflitti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { UserComponent } from './user/user';
+import { DroneComponent } from './drone/drone.component'; // Usiamo quello standard
+import { NotificaComponent } from './notifica/notifica';
 import { DipendenteComponent } from './dipendente/dipendente';
 import { AziendaComponent } from './azienda/azienda';
-import { Carrello } from './carrello/carrello';
-import { HomeComponent } from './home/home';
-import { DroneComponent } from './drone/drone';
-import { NotificaComponent } from './notifica/notifica';
-import { DroneComponent } from './drone/drone.component';
-import { UserComponent } from './user/user';
 import { AutoComponent } from './auto-component/auto-component';
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { UserComponent } from './user/user';
+import { Carrello } from './carrello/carrello';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
   { path: 'drone', component: DroneComponent },
-  {path : 'home', component : HomeComponent},
-  {path : 'user', component : UserComponent},
-  {path : 'drone', component : DroneComponent},
   { path: 'auto', component: AutoComponent },
   { path: 'notifica', component: NotificaComponent },
   { path: 'dipendente', component: DipendenteComponent },
   { path: 'azienda', component: AziendaComponent },
-  { path: 'user', component: UserComponent },
   { path: 'carrello', component: Carrello },
-  { path: '**', redirectTo: 'home' } // Sempre per ultima!
+  { path: '**', redirectTo: 'home' } // Questa deve stare sempre per ULTIMA
 ];
