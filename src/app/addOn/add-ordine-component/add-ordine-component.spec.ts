39f1e6d9148111cac5aec8a70c03e10dd8b6082c
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2026-04-17 14:21:38 +0200

    M:  ordineDto ordine.css ordine.html ordine.ts A: add-ordine-component.css add-ordine-component.html add-ordine-component.spec.ts add-ordine-component.ts

---
+++ src/app/addOn/add-ordine-component/add-ordine-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddOrdineComponent } from './add-ordine-component';
+
+describe('AddOrdineComponent', () => {
+  let component: AddOrdineComponent;
+  let fixture: ComponentFixture<AddOrdineComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddOrdineComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AddOrdineComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/ordine-Component/ordine.ts
+++ src/app/ordine-Component/ordine.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { OrdineDto } from '../Dto/OrdineDto';
 import { ordineService } from '../Service/ordineService';
 import { CommonModule } from '@angular/common';
+import { AddOrdineComponent } from '../addOn/add-ordine-component/add-ordine-component';
 
 @Component({
   selector: 'app-ordine',
-  imports: [CommonModule],
+  imports: [CommonModule, AddOrdineComponent],
   templateUrl: './ordine.html',
   styleUrl: './ordine.css',
   standalone: true,
 })
 export class OrdineComponent implements OnInit{
 
-  service: ordineService;
-  ListOrdini: OrdineDto[] = [];
+  service: ordineService; 
+  ListOrdini= signal<OrdineDto[]>([]);
   ordine: OrdineDto | null = null;;
+  ordini = signal<OrdineDto[]>([]);
+
+  isPopupVisible = signal(false);
+  istoggleAddOrdine = signal(false);
+
+  sortedOrdini = computed(() =>
+    [...this.ordini()].sort((a, b) => a.id - b.id)
+  );
+
+  count = computed(() => {
+    const list = this.ordini();
+    return list.length
+      ? Math.max(...list.map(u => u.id))
+      : 0;
+  });
 
   ngOnInit(){
 
@@ -22,6 +38,13 @@ export class OrdineComponent implements OnInit{
 
   constructor(service: ordineService) {
     this.service = service;
+  }
+
+  getAll() {
+  this.service.getAll().subscribe(ordini => {
+  this.ordine = null;
+  this.ListOrdini.set(ordini);
+  })
   }
 
   findById(id: number) {
@@ -33,20 +56,18 @@ export class OrdineComponent implements OnInit{
   trovaConCostoUguale(costo: number) {
   this.service.trovaConCostoUguale(costo).subscribe(ordini => {
   this.ordine = null;
-  this.ListOrdini = ordini;
-  })
+this.ListOrdini.set(ordini);  })
   }
 
   trovaConNumeroProdottiMaggiore(num_prodotti: number) {
   this.service.trovaConNumeroProdottiMaggiore(num_prodotti).subscribe(ordini => {
-  this.ListOrdini = ordini;
-  this.ordine = null;
+this.ListOrdini.set(ordini);  this.ordine = null;
   });
 }
 
 trovaConIndirizzo(indirizzo: string) {
   this.service.trovaConIndirizzo(indirizzo).subscribe(ordini => {
-  this.ListOrdini = ordini;
+this.ListOrdini.set(ordini);
   this.ordine = null;
   });
 }
@@ -54,42 +75,55 @@ trovaConIndirizzo(indirizzo: string) {
 trovaConCostoMaggiore(costo: number) {
   this.service.trovaConCostoMaggiore(costo).subscribe(ordini => {
   this.ordine = null;
-  this.ListOrdini = ordini;
+this.ListOrdini.set(ordini);
   });
 }
 
 trovaConCostoMinore(costo: number) {
   this.service.trovaConCostoMinore(costo).subscribe(ordini => {
   this.ordine = null;
-  this.ListOrdini = ordini;
+this.ListOrdini.set(ordini);
   });
 }
 
 trovaPerUtente(utente: number) {
   this.service.trovaPerUtente(utente).subscribe(ordini => {
-  this.ListOrdini = ordini;
+this.ListOrdini.set(ordini);
   this.ordine = null;
   });
 }
 
 filtro(numero: number, costo: number) {
   this.service.filtro(numero, costo).subscribe(ordini => {
-  this.ListOrdini = ordini;
-  this.ordine = null;
+this.ListOrdini.set(ordini);  this.ordine = null;
+
 });
 }
 
 ordinaPerCostoDecrescente() {
   this.service.ordinaPerCostoDecrescente().subscribe(ordini => {
-  this.ListOrdini = ordini;
+this.ListOrdini.set(ordini);
   this.ordine = null;
 });
 }
 
 trovaTraDueCosti(min: number, max: number) {
   this.service.trovaTraDueCosti(min, max).subscribe(ordini => {
-  this.ListOrdini = ordini;
+this.ListOrdini.set(ordini);
   this.ordine = null;
   });
 }
+
+  onOrdineCreated(ordine: OrdineDto): void {
+    this.ordini.update(list => [...list, ordine]);
+    console.log('Ordine aggiunto:', ordine);
+  }
+
+  togglePopup(): void {
+    this.isPopupVisible.update(v => !v);
+  }
+
+    toggleAddOrdine(): void {
+    this.istoggleAddOrdine.update(v=> !v);
+  }
 }
